793cf3bce93f655ca02e79b06b49940d1be81e6f
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2019-12-28 19:29:29 -0300

    Add files via upload
    ordem de jogadas implementadas

--- forcaClient.py
+++ forcaClient.py
@@ -1,7 +1,7 @@
 import socket
 
-address = ("localhost",20002)
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+address = ("localhost", 20002)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(address)
 
 print("Cliente ativo, fazendo requisição\n")
@@ -11,7 +11,7 @@ while True:
     
     control = client.recv(2)
     control = control.decode()
-    
+    keeper = 0 #condição para leitura de dados
     if(control == 's'):
         print("-> Jogador 1, informe a categoria de palavras deseja\n")
         category = input()
@@ -20,9 +20,23 @@ while True:
         #print(message.decode())   
         
     while True:
-        letter = input('Uma letra: ')
-        letter = letter.encode()
-        client.send(letter)
+        if keeper:
+        	print('leitura liberada')
+        	letter = input('Uma letra: ')
+	        letter = letter.encode()
+	        client.send(letter)
+	        response = client.recv(4096)
+	        if response.decode() == 'Errou':
+	        	keeper = 0
+	        print(response.decode())
+        else:
+        	print('espere sua vez')
+        	response = client.recv(4096)
+	        if response.decode() == 'Sua vez':
+	        	keeper = 1
+	        print(response.decode())
         
 #except:
 #    client.close()
+	        
+	        
--- forcaServer.py
+++ forcaServer.py
@@ -2,6 +2,8 @@ import socket
 import random
 import threading
 
+contador = 1
+
 #Função responsavel por chegar se a letra está contida na palavra
 
 def check_tentative(letter):
@@ -26,26 +28,37 @@ def change_word(category):
 
 #função responsavel por gerenciar cada cliente threading
 
-def manager_Client(conec,word):
-    
+def manager_Client(conec,word, number_player):
+    global contador
+    global cont_plays
+    first_time = True
     while True:
-        letter = conec.recv(2)
-        letter = letter.decode()
-        
-        print(letter)
-        
-        right,fault_qtd = check_tentative(letter)
-        
-        if(fault_qtd == 0):
-            print("End Game!")
-            break
-        elif(right):
-            print("Acerto!")
-        else:
-            print("Errou! Passando a vez...\n")
-            break
-        
-        print(word_clients)   
+        if number_player == contador:
+            if first_time:
+                conec.send("Sua vez".encode())
+                first_time = False
+            #print("jogador ", number_player, current_player)
+            letter = conec.recv(2)
+            letter = letter.decode()
+            
+            print("---- ",letter)
+            
+            right,fault_qtd = check_tentative(letter)
+            
+            if(fault_qtd == 0):
+                print("End Game!")
+                conec.close()
+                break
+            elif(right):
+                print("Acerto!")
+                conec.send('Acertou'.encode())
+            else:
+                print("Errou! Passando a vez...\n")
+                contador = 1 + contador%cont_plays
+                print("Agora a vez é de ", contador)
+                conec.send('Errou'.encode())
+                first_time = True
+            print(word_clients) 
 
 
 #escopo principal
@@ -57,8 +70,8 @@ words_Server = {"Frutas":['uva','maçã','laranja','banana','pera','mamão','aba
 
 #criando conexão tcp
 
-address = ("localhost",20002)
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+address = ("localhost", 20002)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(address)
 server.listen(5)
 
@@ -70,8 +83,9 @@ print("\n-> Aguardando jogadores....\n")
 
 first_iteration = True
 winner = 0
-cont_plays = 1
+cont_plays = 0
 cont_categories = 1
+current_player = 2
 
 print("-----------------CATEGORIAS-----------------")
 for i in words_Server:
@@ -85,8 +99,8 @@ try:
         if(first_iteration):
             conec.send('s'.encode())
             
-        print("Jogador {a} entrou no jogo...".format(a=cont_plays))
         cont_plays = cont_plays + 1
+        print("Jogador {a} entrou no jogo...".format(a=cont_plays))
         
         if(first_iteration):
             
@@ -101,13 +115,13 @@ try:
                     word_clients.append('_')
                     
             print(word_clients)
-            new_client = threading.Thread(target=manager_Client,args=(conec,word))
+            new_client = threading.Thread(target=manager_Client,args=(conec,word,cont_plays))
             new_client.start()
             
             first_iteration = False
         else:
             conec.send('n'.encode())
-            new_client = threading.Thread(target=manager_Client,args=(conec,word))
+            new_client = threading.Thread(target=manager_Client,args=(conec,word,cont_plays))
             new_client.start()
             #conec.send((','.join(word)).encode())
         
